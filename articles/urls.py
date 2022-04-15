b25738f6f82a30e84fa5ba3b7675fe77d07691a9
q <my@mail.org>
2022-04-15 17:57:51 +1000

    Добавлен simpleJWT и пагинация

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from .views import ArticleListView, ArticleRetieveView
 
 urlpatterns = [
     path('articles/', ArticleListView.as_view()),
     path('articles/<slug:slug>/', ArticleRetieveView.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
-
